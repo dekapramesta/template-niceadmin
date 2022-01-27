42463dab5b6de7789bbe85051eed00d493578ce2
Júlio Rafael <33831855+jmrafael@users.noreply.github.com>
2022-01-27 22:59:54 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
+Route::get('/', function () { return view('login'); });
+Route::get('/register', function () { return view('register'); });
